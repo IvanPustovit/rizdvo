80e5a36255143b23fa5e2eb95a82fb67cb957876
IvanPustovit <pustovit.for@gmail.com>
2024-12-26 14:18:56 +0200

    finfsh

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000
-  }
-})
+  root: './client',
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './client/index.html', // Явно вказуємо вхідну точку
+    },
+  },
+});
+
